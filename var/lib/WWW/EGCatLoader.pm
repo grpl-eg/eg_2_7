d7d9c07a7a2dfc5f8be83000a3249194fdbde679
root <root@blade9.grpl.org>
2014-11-06 14:32:17 -0500

    Perl pieces for opac self lost_card reporting

--- var/lib/WWW/EGCatLoader.pm
+++ var/lib/WWW/EGCatLoader.pm
@@ -192,6 +192,7 @@ sub load {
     return $self->load_myopac_main if $path =~ m|opac/myopac/main|;
     return $self->load_myopac_purchase_request_form if $path =~ m|opac/myopac/purchase_request_form|;
     return $self->load_myopac_purchase_request if $path =~ m|opac/myopac/purchase_request|;
+    return $self->load_myopac_confirm_lost_card if $path =~ m|opac/myopac/confirm_lost|;
     return $self->load_myopac_receipt_email if $path =~ m|opac/myopac/receipt_email|;
     return $self->load_myopac_receipt_print if $path =~ m|opac/myopac/receipt_print|;
     return $self->load_myopac_update_email if $path =~ m|opac/myopac/update_email|;
