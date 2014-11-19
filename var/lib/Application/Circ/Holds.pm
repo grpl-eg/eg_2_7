c17f9ee2b07ca880409b1c00bf9626c40ed23bfb
root <root@blade9.grpl.org>
2014-11-19 10:05:59 -0500

    MeLCat hold status for human consumption

--- var/lib/Application/Circ/Holds.pm
+++ var/lib/Application/Circ/Holds.pm
@@ -1280,13 +1280,15 @@ sub _hold_status {
         return 8;
     }
     return 1 unless $hold->current_copy;
-    return 2 unless $hold->capture_time;
 
     my $copy = $hold->current_copy;
     unless( ref $copy ) {
         $copy = $e->retrieve_asset_copy($hold->current_copy)
             or return $e->event;
     }
+
+    return 9 if ($copy->status =~ /111|112|113|114/);
+    return 2 unless $hold->capture_time;
 
     return 3 if $copy->status == OILS_COPY_STATUS_IN_TRANSIT;
 
